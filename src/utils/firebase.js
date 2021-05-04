012dbfdfa625afc569978cbb8c3b3f389a3cca2c
kritika <kritikasri27@gmail.com>
2021-05-05 04:11:36 +0530

    Hide firebase keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB-77-Q9KtAEzLcaBn-iFI1smkHZpNrIyg",
-  authDomain: "recipe-b7a54.firebaseapp.com",
-  projectId: "recipe-b7a54",
-  storageBucket: "recipe-b7a54.appspot.com",
-  messagingSenderId: "1070029386064",
-  appId: "1:1070029386064:web:32ef5fe7a4dbb303093c55",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
