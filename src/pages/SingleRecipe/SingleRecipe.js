1013d1b8ae970e2871ccf585d12340830d3a8448
kritika <kritikasri27@gmail.com>
2021-05-05 15:16:50 +0530

    Fixed path error

--- src/pages/SingleRecipe/SingleRecipe.js
+++ src/pages/SingleRecipe/SingleRecipe.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import Spinner from "./components/Spinner/Spinner";
+import Spinner from "../../components/Spinner/Spinner";
 import "./SingleRecipe.css";
 
 export default function SingleRecipe() {
